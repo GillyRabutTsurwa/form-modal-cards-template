893e36403f55aa59f3b46415442546d980759215
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-05-21 18:21:30 -0500

    first commit of part 1 to master

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Modal v-bind:showModalProp="showModal" v-on:forwardClick="toggleModal" />
+    <button @click="toggleModal">Open Modal</button>
+    <div class="main">
+      <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+      <div class="card" v-for="currentPerson in people" v-bind:key="currentPerson.id">
+        <div class="card__info">
+          <h3>{{currentPerson.name}} - {{currentPerson.age}}</h3>
+          <h5>{{currentPerson.beltColour}}</h5>
+          <button class="btn">Delete</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Modal: Modal
+  },
+  data() {
+    return {
+      people: [
+        { name: "Yoshi", beltColour: "black", age: 25, id: 1 },
+        { name: "Mario", beltColour: "orange", age: 45, id: 2 },
+        { name: "Luigi", beltColour: "brown", age: 35, id: 3 },
+        { name: "Peach", beltColour: "brown", age: 35, id: 4 },
+        { name: "Daisy", beltColour: "brown", age: 35, id: 5 },
+        { name: "Browser", beltColour: "brown", age: 35, id: 6 },
+        { name: "Toad", beltColour: "brown", age: 35, id: 7 }
+      ],
+      // will be used to close the modal
+      showModal: false
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    }
   }
-}
+};
 </script>
 
 <style>
+@import "./App.css";
 #app {
+  position: fixed;
+  top: 0;
+  left: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  font-size: 1.4rem;
+  width: 100%;
+  height: 100%;
+}
+
+.main {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  justify-content: center;
+  gap: 2rem;
+  width: 100%;
+  height: 100%;
+}
+
+.card__info {
+  height: 30rem;
+  box-shadow: 0 1rem 1rem 0.5rem rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div @click.self="$emit('forwardClick')" v-if="showModalProp" class="modal">
+    <div class="modal__content">
+      <p>I am a modal</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // Just a modified way of declaring props using the prop object vue.
+  // the name of the prop is an object of itself and it has properties of the type of the value and if the value is required. Nothing too new here
+  props: {
+    showModalProp: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.modal {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal__content {
+  width: 50rem;
+  height: 30rem;
+  background-color: #fff;
+  color: #333;
+}
+</style>
